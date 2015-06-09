563b92e47f8f8edd3f27733fcd68e4d44e3931f3
Jack Yeh <jack.yeh@gmail.com>
2015-06-09 14:06:07 -0600

    create Delete action: - find the song object to be deleted - destroy the object - redirect to songs page (index)

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -33,6 +33,12 @@ class SongsController < ApplicationController
     end
   end
 
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+    redirect_to tasks_path
+  end
+
 private
 
   def song_params
