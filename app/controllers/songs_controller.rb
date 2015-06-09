1940202a0279226a233b16103adfd650a49f9b92
Jack Yeh <jack.yeh@gmail.com>
2015-06-09 13:47:49 -0600

    create Create action: - Rails 4 requires that the argument passed through is a private method.   - this private method only permits the song title and song artist to be passed through to the create method.   - this security feature prevents mass assignment through the url and   - it prevents people from doing malicious things to your database/site. - if the song saves successfully - redirect to the page of that song object - else render the new song page

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,4 +10,19 @@ class SongsController < ApplicationController
   def new
     @song = Song.new
   end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+private
+
+  def song_params
+    params.require(:song).permit(:title, :artist)
+  end
 end
